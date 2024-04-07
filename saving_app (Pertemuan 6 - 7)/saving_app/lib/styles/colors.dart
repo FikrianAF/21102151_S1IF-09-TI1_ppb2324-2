9ae934928787a3fb5f42fb7d4508d8610d683cb3
Fikrian Akmal Fauzan <21102151@ittelkom-pwt.ac.id>
2024-04-07 23:15:37 +0700

    saving_app

---
+++ saving_app (Pertemuan 6 - 7)/saving_app/lib/styles/colors.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+const Color kBgColor = Color(0XFFF5EECB);
+const Color kWhite = Color(0XFFFFFFFF);
+const Color kBlack = Color(0XFF000000);
+const Color kKnightBlack = Color(0XFF2C2C2C);
+const Color kMatterhornBlack = Color(0XFF525252);
+const Color kSuvaGray = Color(0XFF868686);
+const Color kDarkGray = Color(0XFFFABABAB);
+const Color kEgyptianBlue = Color(0XFF102693);
+const Color kOrange = Color(0XFFF65725);
+const Color kGrey = Color(0XFF888888);
