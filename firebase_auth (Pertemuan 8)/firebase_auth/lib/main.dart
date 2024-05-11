870e40b8511dc3becb7d4a7ca82b7549b9e4bba2
Fikrian Akmal Fauzan <21102151@ittelkom-pwt.ac.id>
2024-05-11 22:12:44 +0700

    firebase

---
+++ firebase_auth (Pertemuan 8)/firebase_auth/lib/main.dart
@@ -0,0 +1,40 @@
+import 'package:firebase_auth_pertemuan8/bloc/login/login_cubit.dart';
+import 'package:firebase_auth_pertemuan8/bloc/register/register_cubit.dart';
+import 'package:firebase_auth_pertemuan8/ui/splash.dart';
+import 'package:firebase_auth_pertemuan8/utils/routers.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+// import 'package:firebase_auth/bloc/login/login_cubit.dart';
+// import 'package:firebase_auth/ui/splash.dart';
+// import 'package:firebase_auth/utils/routes.dart';
+import 'firebase_options.dart';
+
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MultiBlocProvider(
+      providers: [
+        BlocProvider(create: (context) => LoginCubit())
+        ,BlocProvider(create: (context) => RegisterCubit())
+      ],
+      child: MaterialApp(
+        title: "Praktikum 9",
+        debugShowCheckedModeBanner: false,
+        navigatorKey: NAV_KEY,
+        onGenerateRoute: generateRoute,
+        home: SplashScreen(),
+      ),
+    );
+  }
+}
